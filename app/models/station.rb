f72e7cc0d614e32f26ddce81f4f8301cbac578e0
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-12-12 11:11:04 -0700

    end of work time - have service, view, controller, facade, and model working, in process of working on station displays.

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,9 +1,10 @@
 class Station
 
-  attr_reader :name
+  attr_reader :name, :distance
 
   def initialize(data)
     @name = data["station_name"]
+    @distance = data["distance"]
   end
 
 end
--- spec/features/user_can_search_by_zip_code_spec.rb
+++ spec/features/user_can_search_by_zip_code_spec.rb
@@ -14,8 +14,7 @@ describe "As a user" do
   # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
   # And the stations should be limited to Electric and Propane
   # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
-  it 'sees 10 closest stations' do
-
+  it 'sees a list of station names' do
     visit "/"
     page.fill_in "q", with: 80203
     click_on "Locate"
@@ -25,6 +24,19 @@ describe "As a user" do
 
     within(".stations") do
       expect(page).to have_content("Station name: UDR")
+    end
+  end
+
+  it 'sees 10 stations' do
+    visit "/"
+    page.fill_in "q", with: 80203
+    click_on "Locate"
+
+    stub_request(:get, "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json").
+      to_return(body: File.read("./spec/fixtures/stations_results.json"))
+
+    within(".stations") do
+      expect(page).to have_content("Station name", count: 10)
     end
   end
 end
